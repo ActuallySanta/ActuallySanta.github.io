5ab12c94674d50d11297cf10c4649562278cd6e4
ActuallySanta <SantiBenitezFer@outlook.com>
2025-11-23 13:21:19 -0700

    Began Working on project page reorganization

---
+++ JS/OpenProjectsOnClick.js
@@ -0,0 +1,28 @@
+let GamePageIcons = document.getElementsByClassName("ProjectListItem");
+
+console.log("Script is loaded");
+
+for (let element of GamePageIcons) {
+  console.log("Element: " + element.className);
+  //Add the page open event
+  element.addEventListener("click", (e) => {
+    console.log("Clicked!");
+    // debugger;
+    OpenProjectPage(element.dataset.link);
+  });
+
+  element.addEventListener("mouseover", (e) => {
+    {
+      element.style.backgroundColor = "#e9edc9";
+    }
+
+    element.addEventListener("mouseout", (e) => {
+      element.removeAttribute("style");
+    });
+  });
+}
+
+function OpenProjectPage(link) {
+  console.log(link);
+  window.open(link, "_blank");
+}
